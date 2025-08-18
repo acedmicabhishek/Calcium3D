c55683f1af8a5ca7c40b46cb6c0f1f46e71e1897
acedmicabhishek <acedmicabhishek@gmail.com>
2025-08-18 05:47:30 +0530

    compile and run script

--- run.sh
+++ run.sh
@@ -3,6 +3,11 @@
 cd build
 if [ $? -ne 0 ]; then
     echo "[❌] Failed to change directory to build."
+    echo "-----make sure you compiled the project successfully."
+    echo "-----run chmod +x compile.sh"
+    echo "-----and then run ./compile.sh"
+    echo "-----after that run ./run.sh"
+    echo "[❌][❌][❌][❌][❌]"
     exit 1
 fi
 ./calcium3d
